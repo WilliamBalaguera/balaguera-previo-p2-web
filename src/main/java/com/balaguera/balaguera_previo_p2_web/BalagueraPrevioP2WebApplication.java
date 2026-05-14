f08b0a0c2e004ddfd58a13c78e723c678457cace
William Balaguera <Williamstivenbg@ufps.edu.co>
2026-05-14 06:14:00 -0500

    Initial commit

---
+++ src/main/java/com/balaguera/balaguera_previo_p2_web/BalagueraPrevioP2WebApplication.java
@@ -0,0 +1,13 @@
+package com.balaguera.balaguera_previo_p2_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalagueraPrevioP2WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalagueraPrevioP2WebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/balaguera/balaguera_previo_p2_web/BalagueraPrevioP2WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.balaguera.balaguera_previo_p2_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalagueraPrevioP2WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
